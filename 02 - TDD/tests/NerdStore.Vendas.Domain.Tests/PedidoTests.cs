80d68a72b6598507421b3730da472f05f8e4f85b
unknown <diegoferreira940@gmail.com>
2022-07-12 21:34:46 -0300

    feat: start tdd for Pedido

---
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoTests
+    {
+        [Fact(DisplayName = "Adicionar item novo Pedido")]
+        [Trait("Categoria", "PedidoTests")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            // Arrange
+            var pedido = new Pedido();
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto teste", 2, 100);
+
+            // Act
+            pedido.AdicionarItem(pedidoItem);
+
+            // Assert
+            Assert.Equal(200, pedido.ValorTotal);
+
+        }
+    }
+}
